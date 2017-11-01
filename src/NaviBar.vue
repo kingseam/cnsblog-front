5af11a1e9538c48d848763d044f52360ba98318c
CheolHoJung <fnel159@naver.com>
2017-11-01 22:33:06 +0900

    Fix NaviBar slider toggle button not operate

--- src/NaviBar.vue
+++ src/NaviBar.vue
@@ -3,7 +3,7 @@
     <!-- Logo -->
     <a href="index.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>C</b>NS</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>CNS</b>BLOG</span>
     </a>
@@ -11,7 +11,7 @@
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
       <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
       <!-- Navbar Right Menu -->
