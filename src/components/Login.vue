0c93fd1e73d1bce7af3a5686815307cb516c8055
CheolHoJung <fnel159@naver.com>
2017-11-15 22:19:11 +0900

    Update validation to Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,14 +8,18 @@
     <p class="login-box-msg">Sign in to start your session</p>
 
     <form action="../../index2.html" method="post">
-      <div class="form-group has-feedback">
-        <input v-model.trim="user.user_name" type="email" class="form-control" placeholder="Email">
+      <div class="form-group has-feedback" v-bind:class="{ 'form-group--error': $v.user.user_name.$error }">
+        <input v-model.trim="user.user_name" type="email" class="form-control" placeholder="Email" @input="$v.user.user_name.$touch()">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
-      <div class="form-group has-feedback">
-        <input v-model.trim="user.password" type="password" class="form-control" placeholder="Password">
+      <span class="form-group_message" v-if="!$v.user.user_name.required">필수 항목입니다.</span>
+      <span class="form-group_message" v-if="!$v.user.user_name.email">이메일 형식이 올바르지 않습니다.</span>
+      <div class="form-group has-feedback" v-bind:class="{ 'form-group--error': $v.user.password.$error }">
+        <input v-model.trim="user.password" type="password" class="form-control" placeholder="Password" @input="$v.user.password.$touch()">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
+      <span class="form-group_message" v-if="!$v.user.password.required">필수 항목입니다.</span>
+      <span class="form-group_message" v-if="!$v.user.password.minlength">{{ $v.user.password.$params.minLength.min }}자 이상을 입력하세요.</span>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox">
@@ -26,7 +30,7 @@
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="button" class="btn btn-primary btn-block btn-flat" @click="loginProduct(user)">Log In</button>
+          <button type="button" class="btn btn-primary btn-block btn-flat" @click="$v.user.$invalid ? $v.user.$touch() : loginProduct(user)">Log In</button>
         </div>
         <!-- /.col -->
       </div>
@@ -42,7 +46,7 @@
     <!-- /.social-auth-links -->
 
     <a href="#">I forgot my password</a><br>
-    <a href="/register" class="text-center">Register a new membership</a>
+    <router-link :to="{ name: 'Register' }" class="text-center">Register a new membership</router-link>
 
   </div>
   <!-- /.login-box-body -->
@@ -52,6 +56,7 @@
 
 <script>
 import { mapActions } from 'vuex'
+import { email, required, minLength } from 'vuelidate/lib/validators'
 
 export default {
   data: function () {
@@ -67,6 +72,18 @@ export default {
       'loginProduct',
       'logoutProduct'
     ])
+  },
+  validations: {
+    user: {
+      user_name: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(8)
+      }
+    }
   }
 }
 </script>
